e584466742c34e130ad5cfa62a791b83121f3240
Nguyen Hung <hung0974@gmail.com>
2019-03-21 14:03:35 +0700

    add second screen

---
+++ Alarm Noise/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  Alarm Noise
+//
+//  Created by Nguyễn Hùng on 3/21/19.
+//  Copyright © 2019 Khoi Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
